cff759ac8bbd500ef6a775e0cd4ae5dc568e062f
PraveenB <44827567+Pravdany1406@users.noreply.github.com>
2021-09-23 11:33:45 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -2,16 +2,12 @@
 
 def call(Map config = [:]) {
 
-    sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
-
-
 pipeline {
     agent any
     stages {
         stage('Demo') {
             steps {
-                echo 'Hello world'
-                sayHello(config)
+                sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
             }
         }
     }
